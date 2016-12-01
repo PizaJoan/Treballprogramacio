c7af020bcbd82d317786c70f78ff9cbc8b836668
Joan Piza Ferra <pizajoan97@gmail.com>
2016-12-01 17:53:40 +0100

    Provant de que canvii ABFDEGB per ABCDEFG

--- src/Transposition.java
+++ src/Transposition.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Transposition {
     static String cypher(String s, int dim) {
@@ -30,22 +31,29 @@ public class Transposition {
         int dimy = (int) Math.ceil(s.length() / (double) dim);
         char[][] matrix = new char[dim][dimy];
         int cont = 0;
+        int x = 0;
+        int y = 0;
+        int zero = (dim*dimy) - s.length()-1;
+        System.out.println(zero);
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (cont == s.length()) {
-                    matrix[i][j] = 0;
+            for (int j = 0; j < dimy; j++) {
+                if ( y == zero && x == (dimy-1) ) {
+                    matrix[x][zero] = 0;
+                    zero++;
                     continue;
                 }
-                matrix[i][j] = s.charAt(cont);
-                cont++;
-            }
-        }
-        for (int i = 0; i < s.length() - dim; i++) {
-            for (char[] correcte : matrix) {
-                if (correcte[i] != 0) {
-                    sb.append(correcte[i]);
+                // System.out.println(Arrays.deepToString(matrix));
+                if (cont == s.length()) {
+                    matrix[y][x] = 0;
+                    continue;
                 }
+                matrix[y][x] = s.charAt(cont);
+                cont++;
+                y++;
+                System.out.println(Arrays.deepToString(matrix));
             }
+            y = 0;
+            x++;
         }
         return sb.toString();
     }
